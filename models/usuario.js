c23fb9b5330791ab590de21f4607477d3e5a5549
Frozerblast96 <frozrblast@gmail.com>
2021-03-19 18:31:25 -0300

    Usuarios CRUD listo

---
+++ models/usuario.js
@@ -0,0 +1,49 @@
+const {Schema, model} = require('mongoose');
+
+
+const UsuarioSchema = Schema({
+    nombre:{
+        type: String,
+        required: [true, 'El nombre es obligatorio']
+    },
+    correo:{
+        type: String,
+        required: [true, 'El correo es obligatorio'],
+        unique: true
+    },
+    password:{
+        type: String,
+        required: [true, 'La contraseña es obligatorio']
+    },
+    img:{
+        type: String
+    },
+    rol:{
+        type: String,
+        required: true
+    },
+    estado:{
+        type: Boolean,
+        default: true
+    },
+    google:{
+        type: Boolean,
+        default: false
+    }
+});
+
+UsuarioSchema.methods.toJSON = function(){
+    const {__v, password, ...usuario} = this.toObject();
+    return usuario;
+}
+
+
+/*
+    rol:{
+        type: String,
+        required: true,
+        enum: ['ADMIN_ROLE','USER_ROLE']
+    },
+*/
+
+module.exports = model('Usuario',UsuarioSchema);
